7d5cec6ff8882ffd53d87741b0039c48d8935f5b
Vic Trouble <vic.trouble@gmail.com>
2014-10-20 22:11:52 +0300

    added precise (nanoseconds) example

--- examples/precise.cpp
+++ examples/precise.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include <random>
 
 #include <timeit/timeit.h>
 
+template <typename Random>
+void benchmark(const std::string &engine, size_t iterations)
+{
+    Random rnd;
+    std::cout << engine << " - " << timeit::average<std::chrono::nanoseconds>([&rnd](){ rnd(); }, iterations).count() << " ns" << std::endl;
+}
+
 int main()
 {
-    std::minstd_rand rnd;
-    std::cout << timeit::total<std::chrono::nanoseconds>([&rnd](){ rnd(); }, 10000000).count() << " ns" << std::endl;
-
+    auto iterations = 1000000;
+    benchmark<std::minstd_rand>("minstd_rand");
+    benchmark<std::mt19937>("mt19937");
+    benchmark<std::ranlux24>("ranlux24");
+    benchmark<std::knuth_b>("knuth_b");
     return 0;
 }
